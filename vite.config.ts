6578945adcdc08e3cabc374a2b5a6b8564297414
Henrique Rocha <rochahenrique.1023@gmail.com>
2024-09-15 22:56:01 -0300

    Modificando a funcionalidade do botão de enviar

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import rewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/desenhosDaGi"
+  plugins: [
+    react(),
+    rewriteAll() // Adiciona o plugin aqui
+  ],
+  base: '/DesenhosDaGi/',
+  build: {
+    outDir: 'dist'
+  }
 });

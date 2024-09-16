9f4bd8e9fa2d5aa0417d76f7f3271bbb944e8176
Henrique Rocha <rochahenrique.1023@gmail.com>
2024-09-16 19:50:19 -0300

    Mudança no codigo da pagina

--- src/components/TodasAsFotos.tsx
+++ src/components/TodasAsFotos.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { fotos } from '../assets/fotos';
 import fotoPrincipal from "../assets/images/foto-principal.jpg";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Importar o hook useNavigate
 import compartilhar from '../assets/images/compartilhar.png';
 import '../style/TodasAsFotos.css';
 import '../style/Home.css';
 
 const TodasAsFotos: React.FC = () => {
   const [fotosState] = useState(fotos);
+  const navigate = useNavigate(); // Usar o hook useNavigate para navegação programática
 
   const handleShare = () => {
     const sharedUrl = `${window.location.origin}/desenhosDaGi/`;
@@ -25,6 +26,12 @@ const TodasAsFotos: React.FC = () => {
     }
   };
 
+  // Função para lidar com o clique no link e redirecionar para a página principal
+  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault(); // Prevenir o comportamento padrão do link
+    navigate('/desenhosDaGi/'); // Navegar para a página principal
+  };
+
   return (
     <div className='container-todasAsFotos'>
       <img className='img-principal' src={fotoPrincipal} alt="" />
@@ -33,15 +40,15 @@ const TodasAsFotos: React.FC = () => {
           <h4 className="mensagem-explicacao">Todas as minhas imagens em uma galeria estendida.</h4>
           {fotosState.slice(9, 18).map((foto, index) => (
             <div key={index} className='container-botao'>
-              <Link to={`/foto/${foto.id}`} key={index}>
+              <a href={`/foto/${foto.id}`} onClick={handleLinkClick} key={index}>
                 <img className="img" src={foto.src} alt={foto.alt} />
-              </Link>
+              </a>
               <button 
                 className='botao-compartilhar' 
                 onClick={() => handleShare()}
               >
                 <div className="compartilhar">
-                  <img className="img-compartilhar" src={ compartilhar } alt="" />
+                  <img className="img-compartilhar" src={compartilhar} alt="" />
                   <p>Enviar</p>
                 </div>
               </button>
@@ -51,15 +58,15 @@ const TodasAsFotos: React.FC = () => {
         <div className="imagensSegundoLote">
           {fotosState.slice(0, 9).map((foto, index) => (
             <div key={index} className='container-botao'>
-              <Link to={`/foto/${foto.id}`} key={index}>
+              <a href={`/foto/${foto.id}`} onClick={handleLinkClick} key={index}>
                 <img className="img" src={foto.src} alt={foto.alt} />
-              </Link>
+              </a>
               <button 
                 className='botao-compartilhar' 
                 onClick={() => handleShare()}
               >
                 <div className="compartilhar">
-                  <img className="img-compartilhar" src={ compartilhar } alt="" />
+                  <img className="img-compartilhar" src={compartilhar} alt="" />
                   <p>Enviar</p>
                 </div>
               </button>
